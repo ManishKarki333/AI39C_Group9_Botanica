741f196eef7af97dcfc49441db8c37fae015be93
DleshPrazapatee <dileshpraz10@gmail.com>
2026-05-17 18:34:54 +0545

    Updated project

---
+++ app/routes/auth.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+from app.controllers.auth import AuthController
+class AuthRoutes:
+    def __init__(self):
+        self.bp = Blueprint("auth",__name__)
+        self.controller = AuthController()
+
+    def register(self):
+        self.bp.route("/login",methods=["GET", "POST"])(
+            self.controller.login
+        )
+        self.bp.route("/register",methods=["GET", "POST"])(
+            self.controller.register
+        )
+        self.bp.route("/about",methods=["GET", "POST"])(
+            self.controller.about
+        )
+        self.bp.route("/contact",methods=["GET", "POST"])(
+            self.controller.contact
+        )
+        return self.bp
