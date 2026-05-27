e544cabd149d7cc31c897ca25613267e14180c07
DleshPrazapatee <dileshpraz10@gmail.com>
2026-05-27 13:53:09 +0545

    done

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "random-secret-key")
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
+SECRET_KEY = os.getenv("SECRET_KEY", "")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "")
+MYSQL_USER = os.getenv("MYSQL_USER", "")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "")
