a8ba6b436b6be3304d83e7021eb356517f220f50
mehtab313-clg <mehtab.clg7@gmail.com>
2026-06-19 13:41:15 +0545

    updates

--- run.py
+++ run.py
@@ -2,8 +2,3 @@ from app import create_app
 from app.models.database import Database
 from config import SECRET_KEY
 
-app = create_app()
-
-if __name__ == '__main__':
-    Database.create_tables()
-    app.run(debug=True)
